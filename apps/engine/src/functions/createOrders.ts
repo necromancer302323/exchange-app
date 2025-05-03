5b46b1be1771062620dc263556d51e0eb7d9d1f7
Aaryan Singh <aaryansingh@Aaryans-Mini.lan>
2025-05-03 12:34:36 +0530

    fixing the for loop and the locked logic in engine

--- apps/engine/src/functions/createOrders.ts
+++ apps/engine/src/functions/createOrders.ts
@@ -5,39 +5,44 @@ export function createbid(inrBalance: userBalances[], stockBalance: stockBalance
   for (let i = 0; i < inrBalance.length; i++) {
     if (inrBalance[i].userId == message.userId.id) {
       if (message.price * message.quantity > inrBalance[i].balance - inrBalance[i].Locked) {
-        return { message: "insufficient funds", INR: inrBalance[i] }
+        return { message: "insufficient funds" }
       } else {
         let foundAMatch = false;
         const orderId = Math.random().toString();
         let executedQty = 0
         try {
-          for (let j = 0; j < orderbook.asks.length; j++) {
+
+          for (let j = 0; j < orderbook.asks.length;) {
             if (orderbook.asks[j].price == message.price) {
               const user_inr = inrBalance.findIndex((e) => { return e.userId ==  message.userId.id  })
               const user_stock = stockBalance.findIndex((e) => { return e.userId == message.userId.id });
               const seller_inr = inrBalance.findIndex((e) => { return e.userId == orderbook.asks[j].userId })
               const seller_stock = stockBalance.findIndex((e) => { return e.userId == orderbook.asks[j].userId })
               foundAMatch = true;
-              console.log(executedQty)
               if (orderbook.asks[j].quantity == message.quantity - executedQty) {
-                console.log("in here")
-                inrBalance[user_inr].balance -= message.price * message.quantity
-                inrBalance[seller_inr].balance += message.price * message.quantity
-                stockBalance[user_stock].balance[market || "sol_usdc"] += message.quantity
-                stockBalance[seller_stock].balance[market || "sol_usdc"] -= message.quantity
-                console.log(inrBalance[user_inr].Locked)
-                stockBalance[seller_stock].Locked -= message.quantity
+                inrBalance[user_inr].balance -= message.price *(message.quantity - executedQty)
+                inrBalance[seller_inr].balance += message.price * (message.quantity - executedQty)
+                stockBalance[user_stock].balance[market || "sol_usdc"] += message.quantity - executedQty
+                stockBalance[seller_stock].balance[market || "sol_usdc"] -=message.quantity - executedQty
+                stockBalance[seller_stock].Locked -= message.quantity - executedQty
+                if(executedQty!=0){
+                  inrBalance[user_inr].Locked-=message.price *(message.quantity - executedQty)
+                }
                 orderbook.asks.splice(j, 1);
-                executedQty += message.quantity
+                executedQty += message.quantity -executedQty
                 return { message: "bid succesfull", executedQty }
               } else if (orderbook.asks[j].quantity > message.quantity - executedQty) {
-                inrBalance[user_inr].balance -= message.price * message.quantity
-                inrBalance[seller_inr].balance += message.price * message.quantity
-                stockBalance[user_stock].balance[market || "sol_usdc"] += message.quantity
-                stockBalance[seller_stock].balance[market || "sol_usdc"] -= message.quantity
-                orderbook.asks[j].quantity -= message.quantity
-                stockBalance[seller_stock].Locked -= message.quantity
+                inrBalance[user_inr].balance -= message.price * (message.quantity - executedQty)
+                inrBalance[seller_inr].balance += message.price *(message.quantity - executedQty)
+                stockBalance[user_stock].balance[market || "sol_usdc"] += message.quantity - executedQty
+                stockBalance[seller_stock].balance[market || "sol_usdc"] -= message.quantity - executedQty
+                orderbook.asks[j].quantity -= message.quantity 
+                stockBalance[seller_stock].Locked -= message.quantity - executedQty
+                if(executedQty!=0){
+                  inrBalance[user_inr].Locked-=message.price *(message.quantity - executedQty)
+                }
                 executedQty += message.quantity
+                j++
                 return { message: "bid succesfull  ask changed with bid", orderId, executedQty }
               } else {
                 inrBalance[user_inr].balance -= message.price * orderbook.asks[j].quantity
@@ -47,27 +52,22 @@ export function createbid(inrBalance: userBalances[], stockBalance: stockBalance
                 stockBalance[seller_stock].Locked -= orderbook.asks[j].quantity
                 inrBalance[user_inr].Locked += message.price * (message.quantity - orderbook.asks[j].quantity)
                 executedQty += orderbook.asks[j].quantity
-                orderbook.asks.splice(j, 1)
+                orderbook.asks.splice(j, 1);
+                continue;
               }
             }
-            if(orderbook.asks[j] == orderbook.asks[orderbook.asks.length - 1]){
-              console.log(executedQty)
-              console.log(orderbook.asks[j].quantity)
-              console.log(message.quantity)
-              console.log(orderbook.asks[j].quantity == message.quantity - executedQty)
-            }
-            if (executedQty != 0 && orderbook.asks[j] == orderbook.asks[orderbook.asks.length - 1]) {
-              orderbook.bids.push({
-                price: message.price,
-                quantity: message.quantity - executedQty,
-                orderId: orderId,
-                side: "bid",
-                userId: message.userId.id
-              });
-              return { message: "ask added succesfull some executed", orderId, executedQty }
-            }
           }
-
+          if (executedQty!=0) {
+            orderbook.bids.push({
+              price: message.price,
+              quantity: message.quantity - executedQty,
+              orderId: orderId,
+              side: "bid",
+              userId: message.userId.id
+            });
+            
+            return { message: "ask added succesfull some executed", orderId, executedQty }
+          }
           if (!foundAMatch) {
             orderbook.bids.push({
               price: message.price,
@@ -80,7 +80,6 @@ export function createbid(inrBalance: userBalances[], stockBalance: stockBalance
             return {
               message: "bid succesfull",
               orderId,
-              inrBalance,
               executedQty
             }
           } else {
@@ -103,14 +102,14 @@ export function createAsk(inrBalance: userBalances[], stockBalance: stockBalance
     if (message.userId.id == stockBalance[i].userId) {
       if (message.price * message.quantity > stockBalance[i].balance[market || "sol_usdc"] - stockBalance[i].Locked) {
 
-        return JSON.stringify({ message: "insufficient funds" })
+        return { message: "insufficient funds" ,stockBalance}
 
       } else {
         let foundAMatch = false;
         const orderId = Math.random().toString();
         let executedQty = 0
         try {
-          for (let j = 0; j < orderbook.bids.length; j++) {
+          for (let j = 0; j < orderbook.bids.length;) {
             if (orderbook.bids[j].price == message.price) {
               const user_stock = stockBalance.findIndex((e) => { return e.userId == message.userId.id })
               const user_inr = inrBalance.findIndex((e) => { return e.userId == message.userId.id })
@@ -118,20 +117,29 @@ export function createAsk(inrBalance: userBalances[], stockBalance: stockBalance
               const buyer_stock = stockBalance.findIndex((e) => { return e.userId == orderbook.bids[j].userId })
               foundAMatch = true
               if (orderbook.bids[j].quantity == message.quantity - executedQty) {
-                inrBalance[user_inr].balance += message.price * message.quantity
-                inrBalance[buyer_stock].balance -= message.price * message.quantity
-                stockBalance[buyer_stock].balance[market || "sol_usdc"] += message.quantity
-                stockBalance[user_stock].balance[market || "sol_usdc"] -= message.quantity
-                inrBalance[buyer_inr].Locked -= message.quantity * message.price
+                inrBalance[user_inr].balance += message.price * (message.quantity - executedQty)
+                inrBalance[buyer_stock].balance -= message.price * (message.quantity - executedQty)
+                stockBalance[buyer_stock].balance[market || "sol_usdc"] += message.quantity - executedQty
+                stockBalance[user_stock].balance[market || "sol_usdc"] -=message.quantity - executedQty
+                inrBalance[buyer_inr].Locked -=  message.price* (message.quantity - executedQty)
+                executedQty += message.quantity -executedQty
+                if(executedQty!=0){
+                  stockBalance[user_stock].Locked -= message.quantity - executedQty
+                }
                 orderbook.bids.splice(j, 1);
                 return { message: "ask succesfull", orderId }
               } else if (orderbook.bids[j].quantity > message.quantity - executedQty) {
-                inrBalance[user_inr].balance += message.price * message.quantity
-                inrBalance[buyer_inr].balance -= message.price * message.quantity
-                stockBalance[buyer_stock].balance[market || "sol_usdc"] += message.quantity
-                stockBalance[user_stock].balance[market || "sol_usdc"] -= message.quantity
-                orderbook.bids[j].quantity -= message.quantity
-                inrBalance[buyer_inr].Locked += message.quantity * message.price
+                inrBalance[user_inr].balance += message.price * (message.quantity - executedQty)
+                inrBalance[buyer_inr].balance -= message.price *(message.quantity - executedQty)
+                stockBalance[buyer_stock].balance[market || "sol_usdc"] += (message.quantity - executedQty)
+                stockBalance[user_stock].balance[market || "sol_usdc"] -=(message.quantity - executedQty)
+                orderbook.bids[j].quantity -= message.quantity- executedQty
+                inrBalance[buyer_inr].Locked -= message.price* (message.quantity - executedQty)
+                if(executedQty!=0){
+                  stockBalance[user_stock].Locked -=message.quantity - executedQty
+                }
+                executedQty += message.quantity -executedQty
+                j++
                   return { message: "ask succesfull bid changed with ask", orderId }
               } else {
                 inrBalance[user_inr].balance += message.price * orderbook.bids[j].quantity
@@ -139,22 +147,22 @@ export function createAsk(inrBalance: userBalances[], stockBalance: stockBalance
                 stockBalance[buyer_stock].balance[market || "sol_usdc"] += orderbook.bids[j].quantity
                 stockBalance[user_stock].balance[market || "sol_usdc"] -= orderbook.bids[j].quantity
                 stockBalance[user_stock].Locked += message.price * (message.quantity - orderbook.bids[j].quantity)
-                inrBalance[buyer_inr].Locked -= message.quantity * message.price
+                inrBalance[buyer_inr].Locked -= message.quantity * orderbook.bids[j].quantity
                 executedQty += orderbook.bids[j].quantity
                   stockBalance[user_stock].Locked += message.quantity - orderbook.bids[j].quantity
                 orderbook.bids.splice(j, 1);
               }
-              if (executedQty != message.quantity && orderbook.bids[j] == orderbook.bids[orderbook.bids.length - 1]) {
-                orderbook.asks.push({
-                  price: message.price,
-                  quantity: message.quantity - executedQty,
-                  orderId: orderId,
-                  side: "ask",
-                  userId: message.userId.id
-                });
-                return { message: "ask added succesfull some executed", orderId, executedQty }
-              }
             }
+          }
+          if (executedQty != 0) {
+            orderbook.asks.push({
+              price: message.price,
+              quantity: message.quantity - executedQty,
+              orderId: orderId,
+              side: "ask",
+              userId: message.userId.id
+            });
+            return { message: "ask added succesfull some executed", orderId, executedQty }
           }
           if (!foundAMatch) {
             orderbook.asks.push({
